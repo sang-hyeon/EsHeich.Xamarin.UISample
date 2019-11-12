312a643f91a775b47d3af7eaa875e250685ce927
Sang-hyeon Kim <thwj@me.com>
2019-11-12 23:53:13 +0900

    초기 커밋.

---
+++ EsHeichSample/EsHeichSample/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EsHeichSample
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EsHeichSample/EsHeichSample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EsHeichSample.Models;
+
+namespace EsHeichSample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
