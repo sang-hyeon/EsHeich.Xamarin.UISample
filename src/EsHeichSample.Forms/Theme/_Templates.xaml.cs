4557fbd9381faf9cef0cc6059e336d4dbbff6498
Sang-hyeon Kim <thwj@me.com>
2019-11-13 16:10:04 +0900

    Theme 정의 생성, 작성합니다.

---
+++ src/EsHeichSample.Forms/Theme/_Templates.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EsHeichSample.Forms.Theme
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class _Templates : ResourceDictionary
+    {
+        public _Templates()
+        {
+            InitializeComponent();
+        }
+    }
+}
