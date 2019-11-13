17d39433587f4e3861321e61a07adb3ee2586456
Sang-hyeon Kim <thwj@me.com>
2019-11-14 02:28:06 +0900

    하드코딩 애니메이션을 작성합니다. (개체 지향성이 전혀 없는 하드코딩)

---
+++ src/EsHeichSample.Forms/Triggers/HardCodingAnimation.cs
@@ -0,0 +1,35 @@
+﻿
+namespace EsHeichSample.Forms
+{
+    using System;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+
+    /// <summary>
+    /// 좋은 방법이 아닙니다. 애니메이션을 활용하는 제대로된 계층 구현은
+    /// 시간이 많이 걸리는 관계로 하드코딩합니다.
+    /// </summary>
+    public class HardCodingAnimation : TriggerAction<VisualElement>
+    {
+        public bool IsIn { get; set; }
+        public double Razy_sec { get; set; } = 0.5;
+
+        protected async override void Invoke(VisualElement sender)
+        {
+            if (IsIn)
+            {
+                ViewExtensions.CancelAnimations(sender);
+                await Task.Delay(TimeSpan.FromSeconds(Razy_sec));
+                _ = sender.FadeTo(1, 300, Easing.Linear);
+                _ = sender.TranslateTo(0, -150, 300, Easing.Linear);
+            }
+            else
+            {
+                ViewExtensions.CancelAnimations(sender);
+                await Task.Delay(TimeSpan.FromSeconds(Razy_sec));
+                _ = sender.FadeTo(0, 300, Easing.Linear);
+                _ = sender.TranslateTo(0, 0, 300, Easing.Linear);
+            }
+        }
+    }
+}
