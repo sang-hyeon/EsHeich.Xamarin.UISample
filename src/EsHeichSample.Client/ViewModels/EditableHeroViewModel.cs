72ed006169c65eb386ee57e5e26fc62235d39913
Sang-hyeon Kim <thwj@me.com>
2019-11-15 15:30:45 +0900

    영웅 정보를 수정하는 뷰 로직을 작성합니다.

---
+++ src/EsHeichSample.Client/ViewModels/EditableHeroViewModel.cs
@@ -0,0 +1,93 @@
+﻿
+namespace EsHeichSample.Client.ViewModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using EsHeichSample.Client.Datas;
+    using EsHeichSample.Client.Models;
+
+    public class EditableHeroViewModel : HeroViewModel
+    {
+        #region Properties
+        public new string HeroName 
+        { 
+            get => base.HeroName; 
+            set => base.HeroName = value; 
+        }
+        public new string RealName
+        {
+            get => base.RealName;
+            set => base.RealName = value;
+        }
+        public new string Comment
+        {
+            get => base.Comment;
+            set => base.Comment = value;
+        }
+        public new string HeroName_ko
+        {
+            get => base.HeroName_ko;
+            set => base.HeroName_ko = value;
+        }
+        public new string ImgPath
+        {
+            get => base.ImgPath;
+            set => base.ImgPath = value;
+        }
+        public new HeroRole Role
+        {
+            get => base.Role;
+            set => base.Role = value;
+        }
+        public new string SignatureColor
+        {
+            get => base.SignatureColor;
+            set => base.SignatureColor = value;
+        }
+        #endregion
+
+        public EditableHeroViewModel(Hero hero) 
+            : base(hero)
+        {
+
+        }
+
+        protected virtual bool IsValid(Hero hero)
+        {
+            var properties = typeof(Hero).GetProperties();
+
+            foreach(var prop in properties)
+            foreach(var att in prop.GetCustomAttributes(false))
+            {
+                if(att is ValidationAttribute validation)
+                {
+                    var valid = validation.IsValid(prop.GetValue(hero));
+                    if (valid == false)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public Hero ToModel()
+        {
+            var editedHero = new Hero
+            {
+                ID = this.original.ID,
+                HeroName = this.HeroName,
+                RealName = this.RealName,
+                Summary = this.Comment,
+                HeroImg = this.ImgPath,
+                Role = this.Role,
+                HeroName_ko = this.HeroName_ko,
+                SignatureColor_Hex = this.SignatureColor,
+                SuperPower = this.original.SuperPower
+            };
+
+            if (IsValid(editedHero))
+                return editedHero;
+            else throw new ValidationException("invalidate property");
+        }
+    }
+}
---
+++ tests/EsHeichSample.Client.UnitTests/ViewModelTests/EditableHeroTest.cs
@@ -0,0 +1,109 @@
+﻿
+namespace EsHeichSample.Client.UnitTests.ViewModelTests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Xunit;
+    using Moq;
+    using FluentAssertions;
+    using AutoFixture;
+    using AutoFixture.Xunit2;
+
+    using EsHeichSample.Client.Models;
+    using EsHeichSample.Client.ViewModels;
+    using System.Linq;
+    using System.ComponentModel.DataAnnotations;
+
+    public class EditableHeroTest
+    {
+        /// <summary>
+         /// 생성자는 올바르게 동작합니다.
+         /// </summary>
+        [Theory]
+        [InlineAutoData(false)]
+        [InlineAutoData(true, null)]
+        public void Construct_correctly(bool throwException, Hero hero)
+        {
+            //Arrange
+            Action creating = () => new EditableHeroViewModel(hero);
+            bool exception = false;
+
+            //Action
+            try
+            {
+                creating.Invoke();
+            }
+            catch
+            {
+                exception = true;
+            }
+            finally
+            {
+                //Assert
+                exception.Should().Be(throwException);
+            }
+        }
+
+        /// <summary>
+        /// 생성자는 올바르게 멤버 속성을 초기화 합니다.
+        /// </summary>
+        [Theory]
+        [AutoData]
+        public void Constructor_initialize_properties_correctly(Hero hero)
+        {
+            //Arrange
+            EditableHeroViewModel sut = default;
+
+            //Action
+            sut = new EditableHeroViewModel(hero);
+
+            //Assert
+            sut.HeroName.Should().Be(hero.HeroName);
+            sut.HeroName_ko.Should().Be(hero.HeroName_ko);
+            sut.RealName.Should().Be(hero.RealName);
+            sut.Comment.Should().Be(hero.Summary);
+            sut.Role.Should().Be(hero.Role);
+            sut.SignatureColor.Should().Be(hero.SignatureColor_Hex);
+        }
+
+        /// <summary>
+        /// ToModel은 수정된 속성 정보로 새로운 엔티티를 생성합니다.
+        /// </summary>
+        [Theory]
+        [InlineAutoData("Monkey")]
+        public void ToModel_does_return_editied_entity(string newName, Hero hero)
+        {
+            //Arrange
+            var sut = new EditableHeroViewModel(hero);
+
+            //Action
+            sut.HeroName = newName;
+            var newHero = sut.ToModel();
+
+            //Assert
+            newHero.Should().BeEquivalentTo(hero, option => option.Excluding(x => x.HeroName));
+            newHero.HeroName.Should().Be(newName);
+        }
+
+        /// <summary>
+        /// ToModel은 엔티티의 속성이 유효하지 않으면 장애를 발생합니다.
+        /// </summary>
+        [Theory]
+        [AutoData]
+        public void ToModel_throw_exception_if_invalid_property(string newName, Hero hero)
+        {
+            //Arrange
+            var newHeroName = Enumerable.Range(0, 200).Select(x => newName).Aggregate((a, b) => a + b);
+            var sut = new EditableHeroViewModel(hero);
+
+            //Action
+            sut.HeroName = newHeroName;
+            var toModel = new Action(()=> sut.ToModel());
+
+            //Assert
+            toModel.Should().Throw<ValidationException>();
+        }
+    }
+}
