314e840e6d3c23ac8912e4577a7f63c5a6f3e816
Sang-hyeon Kim <thwj@me.com>
2019-11-16 22:51:22 +0900

    Command 작성합니다.

---
+++ src/EsHeichSample.Client/Helpers/Command.cs
@@ -0,0 +1,49 @@
+﻿
+namespace EsHeichSample.Client
+{
+    using System;
+    using System.Windows.Input;
+
+    public class Command : Command<object>
+    {
+        public Command(Action action, Func<bool> canExecute = null) 
+            : base((_)=> action.Invoke(), (_)=> canExecute.Invoke())
+        {
+        }
+    }
+
+    public class Command<TArg> : ICommand
+    {
+        readonly protected Action<TArg> _action;
+        readonly protected Func<TArg, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<TArg> action, Func<TArg, bool> canExecute = default) 
+        {
+            this._action = action;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is TArg arg)
+                return _canExecute?.Invoke(arg) ?? false;
+            else 
+                return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is TArg arg)
+            {
+                this._action?.Invoke(arg);
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
---
+++ tests/EsHeichSample.Client.UnitTests/HelperTests/CommandTests.cs
@@ -0,0 +1,96 @@
+﻿
+namespace EsHeichSample.Client.UnitTests.HelperTests
+{
+    using System;
+
+    using Xunit;
+    using Moq;
+    using FluentAssertions;
+    using AutoFixture;
+    using AutoFixture.Xunit2;
+
+    using EsHeichSample.Client;
+
+    public class CommandTests
+    {
+
+        [Theory]
+        [InlineAutoData(false, typeof(object))]
+        [InlineAutoData(true, typeof(MockData))]
+        public void Execute_does_invoke_action_correctly(bool executed, Type paramType)
+        {
+            //Arrange
+            var observer = false;
+            var param = Activator.CreateInstance(paramType);
+            var action = new Action<MockData>((x) => observer = true);
+            var sut = new Command<MockData>(action);
+
+            //Action
+            sut.Execute(param);
+
+            //Assert
+            observer.Should().Be(executed);
+        }
+
+        [Theory]
+        [InlineAutoData(false, typeof(object))]
+        [InlineAutoData(true, typeof(MockData))]
+        public void CanExecute_does_return_correctly(bool expect, Type paramType)
+        {
+            //Arrange
+            var param = Activator.CreateInstance(paramType);
+            var action = new Action<MockData>((x) => { });
+            var canExcute = new Func<MockData,bool>((x) => true);
+            var sut = new Command<MockData>(action, canExcute);
+
+            //Action
+            var can = sut.CanExecute(param);
+
+            //Assert
+            can.Should().Be(expect);
+        }
+
+        [Fact]
+        public void raise_CanExecuteChanged_if_can_execute_changed()
+        {
+            //Arrange
+            var action = new Action<MockData>((x) => { });
+            var canExcute = new Func<MockData, bool>((x) => true);
+            var sut = new Command<MockData>(action, canExcute);
+
+            using(var mt = sut.Monitor())
+            {
+                //Action
+                sut.RaiseCanExecuteChanged();
+
+                //Aseert
+                mt.Should().Raise(nameof(Command.CanExecuteChanged));
+            }
+        }
+
+        [Theory]
+        [AutoData]
+        public void replace_parameters_of_Constructor(object param)
+        {
+            //Arrange
+            var observer = false;
+            var observer_canExecute = false;
+
+            var action = new Action(() => observer = true);
+            var canExcute = new Func<bool>(() => { observer_canExecute = true; return true; });
+            var sut = new Command(action, canExcute);
+
+            //Action
+            sut.CanExecute(param);
+            sut.Execute(param);
+
+            //Assert
+            observer.Should().BeTrue();
+            observer_canExecute.Should().BeTrue();
+        }
+
+        public class MockData
+        {
+        }
+    }
+}
